8f3577dcb6a1ebacbf9b56eab5416b02cc5b3142
ryta <Huang_yiho@hotmail.com>
2021-06-02 23:50:57 +0800

    feat: add util,encode.. for axios and fix axios.

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
